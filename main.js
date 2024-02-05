2569e9b9fcf60f96545aa3078b415479438d4e51
gabrielwedin <gabbe_wedin@hotmail.com>
2024-02-05 12:15:54 +0100

    fixed next and previous page

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 let apiKey = "42146396-17c29595aecd509581a48434e";
 let form = document.querySelector('#search-form');
+let currentPage = 1;
+let previousPageButton = document.getElementById('previous-page');
+let nextPageButton = document.getElementById('next-page');
+
 form.addEventListener('submit', async function(event){
     event.preventDefault();
     let searchTerm = document.getElementById('search-term').value;
     let selectedColor = document.getElementById('colors').value;
     let URL;
     if (selectedColor === 'any-color') {
-        URL = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(searchTerm)}&per_page=10`;
+        URL = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(searchTerm)}&page=${currentPage}&per_page=10`;
     }
     else {
-        URL = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(selectedColor + ' ' + searchTerm)}&per_page=10`;
+        URL = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(selectedColor + ' ' + searchTerm)}&page=${currentPage}&per_page=10`;
     }
     let pictures = await getPictures(URL);
     displayImages(pictures.hits);
+
+    if (currentPage > 1) {
+        previousPageButton.style.display = 'block';
+    } else {
+        previousPageButton.style.display = 'none';
+    }
+
+    if (currentPage < Math.ceil(pictures.totalHits / 10)) {
+        nextPageButton.style.display = 'block';
+    } else {
+        nextPageButton.style.display = 'none';
+    }
 });
 
 async function getPictures(URL) {
@@ -47,3 +63,22 @@ function displayImages(images) {
     });
 
 }
+
+function hidePageButtons() {
+    previousPageButton.style.display = 'none';
+    nextPageButton.style.display = 'none';
+}
+
+hidePageButtons();
+
+previousPageButton.addEventListener('click', function() {
+    if (currentPage > 1) {
+        currentPage--;
+        form.dispatchEvent(new Event('submit'));
+    }
+});
+
+nextPageButton.addEventListener('click', function() {
+    currentPage++;
+    form.dispatchEvent(new Event('submit'));
+});
